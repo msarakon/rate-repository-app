bca5d34e38f731ce633fab38346f1632a6a792ed
Minna Sarakontu <minna.sarakontu@aava.fi>
2021-12-28 13:44:23 +0200

    test RepositoryList

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -46,15 +46,17 @@ const styles = StyleSheet.create({
 
 const formatNumber = num => (num >= 1000 ? (num / 1000).toFixed(1) + "k" : num)
 
-const Tag = ({ label }) => (
+const Tag = ({ label, ...props }) => (
   <View style={styles.tag}>
-    <Text style={styles.tagText}>{label}</Text>
+    <Text style={styles.tagText} {...props}>
+      {label}
+    </Text>
   </View>
 )
 
-const Stats = ({ count, label }) => (
+const Stats = ({ count, label, ...props }) => (
   <View style={styles.stats}>
-    <Text style={styles.heading} fontWeight="bold">
+    <Text style={styles.heading} fontWeight="bold" {...props}>
       {formatNumber(count)}
     </Text>
     <Text color="textSecondary">{label}</Text>
@@ -63,25 +65,50 @@ const Stats = ({ count, label }) => (
 
 const RepositoryItem = ({ repository }) => {
   return (
-    <View style={styles.container}>
+    <View style={styles.container} testID="repository-item">
       <View style={styles.topContent}>
         <Image
           style={styles.avatar}
           source={{ uri: repository.ownerAvatarUrl }}
         />
         <View style={styles.info}>
-          <Text fontWeight="bold" style={styles.heading}>
+          <Text
+            fontWeight="bold"
+            style={styles.heading}
+            testID={`${repository.id}-full-name`}
+          >
             {repository.fullName}
           </Text>
-          <Text color="textSecondary">{repository.description}</Text>
-          <Tag label={repository.language} />
+          <Text color="textSecondary" testID={`${repository.id}-description`}>
+            {repository.description}
+          </Text>
+          <Tag
+            label={repository.language}
+            testID={`${repository.id}-language`}
+          />
         </View>
       </View>
       <View style={styles.bottomContent}>
-        <Stats count={repository.stargazersCount} label="Stars" />
-        <Stats count={repository.forksCount} label="Forks" />
-        <Stats count={repository.reviewCount} label="Reviews" />
-        <Stats count={repository.ratingAverage} label="Rating" />
+        <Stats
+          count={repository.stargazersCount}
+          label="Stars"
+          testID={`${repository.id}-stars`}
+        />
+        <Stats
+          count={repository.forksCount}
+          label="Forks"
+          testID={`${repository.id}-forks`}
+        />
+        <Stats
+          count={repository.reviewCount}
+          label="Reviews"
+          testID={`${repository.id}-reviews`}
+        />
+        <Stats
+          count={repository.ratingAverage}
+          label="Rating"
+          testID={`${repository.id}-rating`}
+        />
       </View>
     </View>
   )
